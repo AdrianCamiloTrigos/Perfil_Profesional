bec68793f27e13bf961d752a6757fa747d9ccfbf
AdrianCamiloTrigos <adriangamer0319@gmail.com>
2023-11-03 14:21:52 -0500

    Botones JS

--- JS/function.js
+++ JS/function.js
@@ -73,8 +73,80 @@ console.log( "divison " + division.toFixed(2));
 var modulo = number_one % number_two;
 console.log("modulo " + modulo);
 
+// OPERADORES LOGICOS Y EWSTRUCTURAS CONDICIONALES (INVERSE, INCREMENTO Y DECREMENTO)
 
+// AND && con if - Ampersand
 
+var bool = false
+numeric = 10;
+if(!bool && numeric === "10") { //bool == true
+  console.log("ENTRA IF")
+}
+else{
+  console.log("ENTRA ELSE");
+}
+// or || con if
+if(!bool || numeric == 7){
+  console.log("entra if");
+  numeric+=3;
+}else{
+  console.log("entra else");
+  numeric--;
+}
+console.log(numeric);
 
+// FOR
+for(let j = 0; j<Array_text.lenght; j++){
+  console.log(Array_text[j] + (j+1));
+}
 
+//WHILE
+let f=0;
+while(f<Array_text.length){
+  console.log(Array_text[f] + (f+1));
+  f++;
+}
+
+// DO WHILE
+let m=0;
+do{
+  console.log(Array_text[m] + (m+1));
+  m++;
+}while(m<Array_text.length);
+
+//FUNCIONES Y EVENTOS
+function load_page(){
+  //alert("la pagina se ha cargado correctamente");
+}
+
+function change_Color(){
+  document.body.style.backgroundColor = "red";
+  document.body.style.color = "#fff"
+}
+
+const btn_Limpar = document.querySelector("#clear_color");
+
+clear_color.addEventListener("dblclick", () => {
+  document.body.style.backgroundColor = "#fff";
+  document.body.style.color = "#000"
+});
+
+//Registro formulario
+const form_register = document.getElementById("form_register");
+const nombres       = document.getElementById("nombres");
+const apellidos       = document.getElementById("apellidos");
+const information       = document.getElementById("information");
+
+form_register.addEventListener("submit" , name_event =>  {
+  name_event.preventDefault();
+  let info ="";
+  //console.log(nombres.value);
+  if(nombres.ariaValueMax.length <= 2 || apellidos.ariaValueMax.length <= 2){
+      info += "Nombres Y/O APELLIDOS INVALIDOS"
+      information.style.color = "red";
+  }else{
+      alert(nombres.value + " " + apellidos)
+  }
+  information.innerText = info;
+})
 
